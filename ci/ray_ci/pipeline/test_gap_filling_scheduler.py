cbc53a81994f55ad48769fd008ae2ba5609597fe
can <can@anyscale.com>
2024-04-11 02:41:42 +0000

    [ci][postmerge/3] actually trigger build in commit gaps
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/pipeline/test_gap_filling_scheduler.py
+++ ci/ray_ci/pipeline/test_gap_filling_scheduler.py
@@ -84,52 +84,5 @@ def test_run(mock_get_gap_commits, mock_trigger_build):
     )
 
 
-@mock.patch("subprocess.check_output")
-@mock.patch(
-    "ci.ray_ci.pipeline.gap_filling_scheduler.GapFillingScheduler._get_latest_commits"
-)
-@mock.patch("ci.ray_ci.pipeline.gap_filling_scheduler.GapFillingScheduler._get_builds")
-def test_get_gap_commits(mock_get_builds, mock_get_latest_commits, mock_check_output):
-    def _mock_check_output_side_effect(cmd: str, shell: bool) -> str:
-        assert cmd == "git rev-list --reverse ^111 444~"
-        return b"222\n333\n"
-
-    mock_get_builds.return_value = [
-        {
-            "state": "passed",
-            "commit": "111",
-        },
-        {
-            "state": "failed",
-            "commit": "444",
-        },
-    ]
-    mock_get_latest_commits.return_value = [
-        "444",
-        "111",
-    ]
-    mock_check_output.side_effect = _mock_check_output_side_effect
-    scheduler = GapFillingScheduler("org", "pipeline", "token")
-    assert scheduler.get_gap_commits() == ["222", "333"]
-
-
-@mock.patch(
-    "ci.ray_ci.pipeline.gap_filling_scheduler.GapFillingScheduler._trigger_build"
-)
-@mock.patch(
-    "ci.ray_ci.pipeline.gap_filling_scheduler.GapFillingScheduler.get_gap_commits"
-)
-def test_run(mock_get_gap_commits, mock_trigger_build):
-    mock_get_gap_commits.return_value = ["222", "333"]
-    scheduler = GapFillingScheduler("org", "pipeline", "token")
-    scheduler.run()
-    mock_trigger_build.assert_has_calls(
-        [
-            mock.call("222"),
-            mock.call("333"),
-        ]
-    )
-
-
 if __name__ == "__main__":
     sys.exit(pytest.main(["-v", __file__]))
